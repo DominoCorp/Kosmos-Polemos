0945860f0ade2bffd03168a62cef183a7a9f4763
Domino1234 <122507153+Domino1234B@users.noreply.github.com>
2024-06-21 13:16:37 -0700

    Update eaglercraft_opts.js

--- web/eaglercraft_opts.js
+++ web/eaglercraft_opts.js
@@ -3,8 +3,8 @@ window.eaglercraftXOpts = () => ({
   // joinServer: "ws" + window.location.href.slice(4),
   // assetsURI: "custom.epk",
 	servers: [
-		{ addr: "ws" + window.location.href.slice(4), name: "EaglercraftX Server" }
+		{ addr: "ws" + window.location.href.slice(4), name: "Kosmos Polemos" }
 	]
 });
 // document.title = "My Original Server Name:tm:";
-// document.querySelector("link[rel=\"shortcut icon\"]").href = "https://www.google.com/favicon.ico";
+// document.querySelector("link[rel=\"shortcut icon\"]").href = "https://images.squarespace-cdn.com/content/v1/5e7b75f9c579482846587cb3/439b7baf-225d-41a3-96f5-119aac0c6900/IOPT+world.jpg";
